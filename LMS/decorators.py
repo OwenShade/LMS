486b5f0825ad017b2e353f449ecee59a2f5d36a3
Johnathan Sciallo <2326843s@student.gla.ac.uk>
2020-03-31 14:38:52 +0100

    Decorators comments and error message fix

--- LMS/decorators.py
+++ LMS/decorators.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+#Checks if current user is authenticated and if so,
+# returns them to the home screen,
+#else allows them to view the page
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -10,6 +13,9 @@ def unauthenticated_user(view_func):
 
     return wrapper_func
 
+#Checks if user is in one or more ofthe allowed
+# groups for this view, if so allows them to view,
+# else returns an error page
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
@@ -21,7 +27,7 @@ def allowed_users(allowed_roles=[]):
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
             else:
-                return HttpResponse('You are not authorized to view this page.')
+                return HttpResponse('You are not authorised to view this page.')
             return view_func(request, *args, **kwargs)
         return wrapper_func
     return decorator
