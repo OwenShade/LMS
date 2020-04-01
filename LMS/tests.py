21bf9d30afe7af3f418bb7432ab804d9f7353f70
Owen Shade <2378587s@student.gla.ac.uk>
2020-04-01 22:01:05 +0100

    few more fixes

--- LMS/tests.py
+++ LMS/tests.py
@@ -26,14 +26,14 @@ class CategoryMethodTests(TestCase):
 
         self.assertEqual(category.slug, 'classic-works')
 
-""" class ISBNMethodTests(TestCase):
+class ISBNMethodTests(TestCase):
     def test_ensure_views_are_positive(self):
         
 
         isbn = ISBN(ISBN= 01234567, views=-1)
         category.save()
 
-        self.assertEqual((category.views >= 0 ), True)  """
+        self.assertEqual((category.views >= 0 ), True)
 
 class SearchViewTests(TestCase):
     def test_search_view_with_no_categories(self):
@@ -42,7 +42,7 @@ class SearchViewTests(TestCase):
         displays a view to reflect this
         """
 
-        response = self.client.get(reverse('/browse'))
+        response = self.client.get(reverse('LMS:browse'))
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'There are no categories present.')
@@ -62,11 +62,11 @@ class SearchViewTests(TestCase):
         checks that the correct number of categories are present
         """
 
-        add_category('General Works')
-        add_category('Drama')
-        add_category('Philosophy')
+        self.add_category('General Works')
+        self.add_category('Drama')
+        self.add_category('Philosophy')
 
-        response = self.client.get(reverse('/browse'))
+        response = self.client.get(reverse('LMS:browse'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "General Works")
         self.assertContains(response, "Drama")
@@ -77,8 +77,8 @@ class SearchViewTests(TestCase):
     
     def test_category_that_already_exists_cannot_be_added_again(self):
         
-        response = self.client.get(reverse('/browse'))
-        add_category('General Works')
+        response = self.client.get(reverse('LMS:browse'))
+        self.add_category('General Works')
         self.assertEquals(response.status_code, 302)
 
 class CategorySlugViewTests(TestCase):
@@ -89,7 +89,7 @@ class CategorySlugViewTests(TestCase):
         displays a view to reflect this
         """
 
-        response = self.client.get(reverse('/browse/general-works'))
+        response = self.client.get(reverse('LMS:browse/general-works'))
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'There are no books present.')
@@ -108,11 +108,11 @@ class CategorySlugViewTests(TestCase):
 
     def test_category_slug_with_books_present(self):
 
-        add_ISBN(1000000, "General Works", "A. book", "A woman", "Romance")
-        add_ISBN(1000001, "Drama", "Another Book", "A. notherwoman", "Murder Mystery")
-        add_ISBN(1000002, "Philosophy", "A Final Book", "A. finalwoman", "Science Fiction")
+        self.add_ISBN(1000000, "General Works", "A. book", "A woman", "Romance")
+        self.add_ISBN(1000001, "Drama", "Another Book", "A. notherwoman", "Murder Mystery")
+        self.add_ISBN(1000002, "Philosophy", "A Final Book", "A. finalwoman", "Science Fiction")
 
-        response = self.client.get(reverse('/browse'))
+        response = self.client.get(reverse('LMS:browse'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "name")
         self.assertContains(response, "name")
@@ -124,19 +124,19 @@ class CategorySlugViewTests(TestCase):
 class UserAuthenticationTests(TestCase):
 
     def test_unauthenticated_user_can_access_home(self):
-        response = self.client.get(reverse('/LMS'))
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
     
     def test_unauthenticated_user_can_access_browse(self):
-        response = self.client.get(reverse('/browse'))
+        response = self.client.get(reverse('LMS:browse'))
         self.assertEqual(response.status_code, 200)
 
     def test_unauthenticated_user_can_access_search(self):
-        response = self.client.get(reverse('/search'))
+        response = self.client.get(reverse('LMS:search'))
         self.assertEqual(response.status_code, 200)
     
     def test_unauthenticated_user_cannot_access_staffpage(self):
-        response = self.client.get(reverse('/staff_page'))
+        response = self.client.get(reverse('LMS:staff_page'))
         self.assertEqual(response.status_code, 302)
 
 class UserLoginOutTests(TestCase):
@@ -156,9 +156,9 @@ class UserLoginOutTests(TestCase):
 
     def test_user_logs_in_successfully(self):
 
-        add_user("TestUser", "TestUsersPassword@2000")
+        self.add_user("TestUser", "TestUsersPassword@2000")
 
-        response = self.client.post(reverse('/login'),
+        response = self.client.post(reverse('LMS:login'),
                                     {'username': 'TestUser',
                                     'password': 'TestUsersPassword@2000'})
         self.assertEqual(response.status_code, 200)
