d1bd515e4b24cfbd1760fa511d72717a695f01c7
Owen Shade <2378587s@student.gla.ac.uk>
2020-04-01 21:25:58 +0100

    tests finished just need to get the syntax right so they run

--- LMS/tests.py
+++ LMS/tests.py
@@ -26,16 +26,14 @@ class CategoryMethodTests(TestCase):
 
         self.assertEqual(category.slug, 'classic-works')
 
-class ISBNMethodTests(TestCase):
+""" class ISBNMethodTests(TestCase):
     def test_ensure_views_are_positive(self):
-        """
-        Ensures the number of views on a given books ISBN is not negative
-        """
+        
 
-        isbn = ISBN(ISBN= 01234567890, views=-1)
+        isbn = ISBN(ISBN= 01234567, views=-1)
         category.save()
 
-        self.assertEqual((category.views >= 0 ), True)
+        self.assertEqual((category.views >= 0 ), True)  """
 
 class SearchViewTests(TestCase):
     def test_search_view_with_no_categories(self):
@@ -44,7 +42,7 @@ class SearchViewTests(TestCase):
         displays a view to reflect this
         """
 
-        response = self.client.get(reverse('/LMS/browse'))
+        response = self.client.get(reverse('/browse'))
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'There are no categories present.')
@@ -68,7 +66,7 @@ class SearchViewTests(TestCase):
         add_category('Drama')
         add_category('Philosophy')
 
-        response = self.client.get(reverse('/LMS/browse'))
+        response = self.client.get(reverse('/browse'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "General Works")
         self.assertContains(response, "Drama")
@@ -79,6 +77,7 @@ class SearchViewTests(TestCase):
     
     def test_category_that_already_exists_cannot_be_added_again(self):
         
+        response = self.client.get(reverse('/browse'))
         add_category('General Works')
         self.assertEquals(response.status_code, 302)
 
@@ -90,26 +89,30 @@ class CategorySlugViewTests(TestCase):
         displays a view to reflect this
         """
 
-        response = self.client.get(reverse('/LMS/browse/general-works'))
+        response = self.client.get(reverse('/browse/general-works'))
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'There are no books present.')
         self.assertQuerysetEqual(response.context['ISBN'], [])
 
-    def add_ISBN():
+    def add_ISBN(ISBN, category, title, author, genre, views = 0):
         """
-        This method should add an instance of an ISBN according to the ISBN model
+        This method adds an instance of an ISBN according to the ISBN model
         """
-
-
+        isbn = ISBN.objects.get_or_create(isbn=ISBN)[0]
+        isbn.category = category
+        isbn.title = title
+        isbn.author = author
+        isbn.genre = genre
+        isbn.views = views
 
     def test_category_slug_with_books_present(self):
 
-        add_ISBN()
-        add_ISBN()
-        add_ISBN()
+        add_ISBN(1000000, "General Works", "A. book", "A woman", "Romance")
+        add_ISBN(1000001, "Drama", "Another Book", "A. notherwoman", "Murder Mystery")
+        add_ISBN(1000002, "Philosophy", "A Final Book", "A. finalwoman", "Science Fiction")
 
-        response = self.client.get(reverse('/LMS/browse'))
+        response = self.client.get(reverse('/browse'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "name")
         self.assertContains(response, "name")
@@ -125,32 +128,39 @@ class UserAuthenticationTests(TestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_unauthenticated_user_can_access_browse(self):
-        response = self.client.get(reverse('/LMS/browse'))
+        response = self.client.get(reverse('/browse'))
         self.assertEqual(response.status_code, 200)
 
     def test_unauthenticated_user_can_access_search(self):
-        response = self.client.get(reverse('/LMS/search'))
+        response = self.client.get(reverse('/search'))
         self.assertEqual(response.status_code, 200)
     
     def test_unauthenticated_user_cannot_access_staffpage(self):
-        response = self.client.get(reverse('/LMS/staff_page'))
+        response = self.client.get(reverse('/staff_page'))
         self.assertEqual(response.status_code, 302)
 
 class UserLoginOutTests(TestCase):
 
-    def add_user():
+    def add_user(user, password, book_limit=10):
         """
-        This method should add an instance of a user
+        This method adds an instance of a user
         according to the user model
         """
+        member = Member.objects.get_or_create(user=user)[0]
+        member.book_limit = book_limit
+        member.password = password
+        
+        member.save()
+        return member
+
 
     def test_user_logs_in_successfully(self):
 
-        add_user()
+        add_user("TestUser", "TestUsersPassword@2000")
 
-        response = self.client.post(reverse('/LMS/login'),
-                                    {'username': 'username',
-                                    'password': 'password'})
+        response = self.client.post(reverse('/login'),
+                                    {'username': 'TestUser',
+                                    'password': 'TestUsersPassword@2000'})
         self.assertEqual(response.status_code, 200)
         user = auth.get_user(self.client)
         assert user.is_authenticated()
